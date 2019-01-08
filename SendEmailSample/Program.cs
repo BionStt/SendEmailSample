3b2c60966e551a368860cef8cf1364e6c82a284a
levmin <lminkovsky@outlook.com>
2019-01-08 14:40:30 -0500

    Add project files.

---
+++ SendEmailSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SendEmailSample
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
